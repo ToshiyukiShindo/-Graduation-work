0008a7863fe89c0f5a28d821b5bc7e508485b7a9
ToshiyukiShindo <fight.holding.water@gmail.com>
2022-04-20 16:04:11 +0000

    “message”

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
@@ -14,8 +15,14 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-            DB::table('users')->insert([
-            'password' => Hash::make('password'),
-            ]);
+            // $users = new User;
+            // User::create([
+            // 'id'=>'2',
+            // 'name'=>'Master',
+            // 'email'=>'master@test.com',
+            // 'permission'=>'1',
+            // 'org'=>'all',
+            // 'password'=>Hash::make('rootroot'),
+            // ]);
     }
 }
